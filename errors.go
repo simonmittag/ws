9607e30a91c810bd546f1873eb67d4bd1b6bc4e1
simon.mittag <simonmittag@gmail.com>
2021-04-20 08:20:28 +1000

    exported error type

--- errors.go
+++ errors.go
@@ -2,12 +2,12 @@ package ws
 
 // RejectOption represents an option used to control the way connection is
 // rejected.
-type RejectOption func(*rejectConnectionError)
+type RejectOption func(*RejectConnectionErrorType)
 
 // RejectionReason returns an option that makes connection to be rejected with
 // given reason.
 func RejectionReason(reason string) RejectOption {
-	return func(err *rejectConnectionError) {
+	return func(err *RejectConnectionErrorType) {
 		err.reason = reason
 	}
 }
@@ -15,7 +15,7 @@ func RejectionReason(reason string) RejectOption {
 // RejectionStatus returns an option that makes connection to be rejected with
 // given HTTP status code.
 func RejectionStatus(code int) RejectOption {
-	return func(err *rejectConnectionError) {
+	return func(err *RejectConnectionErrorType) {
 		err.code = code
 	}
 }
@@ -23,7 +23,7 @@ func RejectionStatus(code int) RejectOption {
 // RejectionHeader returns an option that makes connection to be rejected with
 // given HTTP headers.
 func RejectionHeader(h HandshakeHeader) RejectOption {
-	return func(err *rejectConnectionError) {
+	return func(err *RejectConnectionErrorType) {
 		err.header = h
 	}
 }
@@ -31,24 +31,24 @@ func RejectionHeader(h HandshakeHeader) RejectOption {
 // RejectConnectionError constructs an error that could be used to control the way
 // handshake is rejected by Upgrader.
 func RejectConnectionError(options ...RejectOption) error {
-	err := new(rejectConnectionError)
+	err := new(RejectConnectionErrorType)
 	for _, opt := range options {
 		opt(err)
 	}
 	return err
 }
 
-// rejectConnectionError represents a rejection of upgrade error.
+// RejectConnectionErrorType represents a rejection of upgrade error.
 //
 // It can be returned by Upgrader's On* hooks to control the way WebSocket
 // handshake is rejected.
-type rejectConnectionError struct {
+type RejectConnectionErrorType struct {
 	reason string
 	code   int
 	header HandshakeHeader
 }
 
 // Error implements error interface.
-func (r *rejectConnectionError) Error() string {
+func (r *RejectConnectionErrorType) Error() string {
 	return r.reason
 }
